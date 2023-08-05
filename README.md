c31f23fbed066c0e5c9b8d9e4426d25a9524ecb4
Giovane Santos Silva <giovanesantos1999@gmail.com>
2023-08-05 11:29:30 -0300

    init: setup project

---
+++ README.md
@@ -0,0 +1,4 @@
+# Video.js AmbiLight
+
+> Simple plugin ambilight effect in video.js player
+
---
+++ public/index.js
@@ -0,0 +1,7 @@
+import 'video.js/dist/video-js.css';
+
+import videojs from "video.js";
+
+window.onload = () => {
+	const video = videojs('my-video');
+}
